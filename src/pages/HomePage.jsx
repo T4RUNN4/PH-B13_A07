809125ec46be1d40dc491fe5d8c34254d49347a6
Tarunna <tarunnasen@gmail.com>
2026-04-14 14:50:35 +0600

    Status UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import Hero from "../components/unique/Hero";
+import StatusContainer from "../components/unique/StatusContainer";
 
 export default function HomePage() {
     return (
         <main className="py-40 px-60 bg-[#F8FAFC]">
             <Hero></Hero>
+            <StatusContainer></StatusContainer>
+            <div class="divider"></div>
         </main>
     )
 }
