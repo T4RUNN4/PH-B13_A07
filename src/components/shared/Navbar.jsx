d799b7b6d4a4f3587dbc45e958870c2e75c935ca
Tarunna <tarunnasen@gmail.com>
2026-04-14 23:18:30 +0600

    Improved UX of Nav and

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -9,9 +9,9 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-sm px-6 md:px-20 py-4 flex flex-col md:flex-row items-center justify-center md:justify-between gap-4">
       <div>
-        <h1 className="text-2xl font-semibold text-[#244D3F]">
+        <Link to="/" onClick={() => setCurrTab("home")} className="text-2xl font-semibold text-[#244D3F]">
           <span className="text-[#1F2937] font-extrabold">Keen</span>Keeper
-        </h1>
+        </Link>
       </div>
       <div>
         <button onClick={() => setCurrTab("home")}>
