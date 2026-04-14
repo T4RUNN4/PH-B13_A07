fef74f8514fcc9e93cb06078f32da33bc6419b51
Tarunna <tarunnasen@gmail.com>
2026-04-14 17:14:04 +0600

    Friends Card UI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,21 @@
+import { use } from "react";
+import FriendsContainer from "../components/unique/FriendsContainer";
 import Hero from "../components/unique/Hero";
 import StatusContainer from "../components/unique/StatusContainer";
 
+const friendsPromise = fetch("/friends.json").then(res => res.json());
+
 export default function HomePage() {
+    const friends = use(friendsPromise);
+    let total = 0, ontrack = 0, overdue = 0, thisMonth = 0;
+
+
     return (
-        <main className="py-40 px-60 bg-[#F8FAFC]">
+        <main className="p-40 bg-[#F8FAFC]">
             <Hero></Hero>
             <StatusContainer></StatusContainer>
-            <div class="divider"></div>
+            <div class="divider my-10"></div>
+            <FriendsContainer friends={friends}></FriendsContainer>
         </main>
     )
 }
